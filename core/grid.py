110184aab9dd91d0d23a29342d8276d3c6a25fb4
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-05-15 17:19:53 -0300

    grid implemented

--- core/grid.py
+++ core/grid.py
@@ -1,69 +1,93 @@
 from PPlay.window import Window
+from PPlay.keyboard import Keyboard
 from PPlay.gameobject import GameObject
 
 from grid_objects.air import Air
 from grid_objects.ground import Ground
-from grid_objects.extense_object import ExtenseObject
-from utils import ScreenUtils
-from entities.player import Player
+from common import GlobalData as GD
 
 import json
 
 class Grid(GameObject):
-    def __init__(self, window: Window, width, height, speed, cell_size, active = True, draw_grid = False) -> None:
+    def __init__(self, width, height, speed, cell_size, active = True, draw_grid = False) -> None:
         super().__init__()
-        self.window = window
         self.matrix = [[None for _ in range(height)] for _ in range(width)]
         self.cell_size = cell_size
         self.x = 0
-        self.y = window.height - (height * cell_size) # Ultima linha fica alinhada ao chão da janela
+        self.y = GD.window.height - (height * cell_size) # Ultima linha fica alinhada ao chão da janela
         self.width = width
         self.height = height
         self.speed = speed
         self.draw_grid = draw_grid
-        self.active = active
-        self.screen = ScreenUtils(window)
+        self.active = active # Determina se o player está em seu plano ou não
 
-    def load_terrain(self, obj_list, obj_class, sprite_path):
-        for obj in obj_list:
-            x = obj["x"]
-            y = obj["y"]
-            width = obj["width"]
-            height = obj["height"]
-
-            obj = ExtenseObject(
-                window=self.window, 
-                x=self.x + x * self.cell_size, 
+    def load_ground(self, obj_list):
+        for item in obj_list:
+            x = item["x"]
+            y = item["y"]
+            width = item["width"]
+            height = item["height"]
+            self.matrix[x][y] = Ground(
+                window=GD.window,
+                x=self.x + x * self.cell_size,
                 y=self.y + (self.height - 1 - y) * self.cell_size,
                 width=width, 
                 height=height,
-                cell_size=self.cell_size,
-                type=obj_class,
-                args=(self.window, sprite_path)
+                tile_size=self.cell_size * 8,
+                active=self.active
             )
-            self.matrix[x][y] = obj
+
+    def load_air(self, obj_list):
+        for item in obj_list:
+            x = item["x"]
+            y = item["y"]
+            width = item["width"]
+            height = item["height"]
+            self.matrix[x][y] = Air(
+                window=GD.window,
+                x=self.x + x * self.cell_size,
+                y=self.y + (self.height - 1 - y) * self.cell_size,
+                width=width, 
+                height=height,
+                tile_size=self.cell_size,
+                active=self.active
+            )
 
     def load_level(self, file_path):
         with open(file_path, "r") as json_file:
             level: dict = json.load(json_file)
+        if self.draw_grid:
+            self.load_air([{"x": 0, "y": self.height - 1, "width": self.width, "height": self.height}])
         for key in level.keys():
-            if key == "Air" and self.draw_grid:
-                self.load_terrain(level[key], Air, "assets/terrain/air.png")
-            elif key == "Ground":
-                self.load_terrain(level[key], Ground, "assets/terrain/ground.png")
+            if key == "Ground":
+                self.load_ground(level[key])
 
-    def update(self, player: Player):
-        self.x -= self.speed * self.window.delta_time()
+    def activate(self):
+        self.active = True
         for obj_list in self.matrix:
-            for j, obj in enumerate(obj_list):
+            for obj in obj_list:
                 if obj:
-                    obj.x -= self.speed * self.window.delta_time()
-                    if self.screen.on_screen(obj):
-                        obj.update(player)
+                    obj.active = True
 
+    def deactivate(self):
+        self.active = False
+        for obj_list in self.matrix:
+            for obj in obj_list:
+                if obj:
+                    obj.active = False
+
+    def update(self):
+        self.x -= self.speed * GD.window.delta_time()
+        for obj_list in self.matrix:
+            for obj in obj_list:
+                if obj:
+                    obj.x -= self.speed * GD.window.delta_time()
+                    if GD.on_screen(obj):
+                        obj.update()
+            
     def draw(self):
         for obj_list in self.matrix:
             for obj in obj_list:
                 if obj:
-                    if self.screen.on_screen(obj):
+                    if GD.on_screen(obj, append_to_list=True):
                         obj.draw()
