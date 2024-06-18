1dda761e375bcab3596c19e079e9b9f7ab5c7379
Gabriel Brevilieri <gbrevilieri@gmail.com>
2024-06-18 16:56:28 -0300

    added title screen while loading

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PPlay.window import Window
+from PPlay.gameimage import GameImage
 
 from core.global_data import GlobalData as GD
 
@@ -7,8 +8,13 @@ from scenes.menu import Menu
 window = Window(1300, 800)
 window.set_title("Slash Run")
 
+title_screen = GameImage("assets/title_screen.png")
+title_screen.set_position(window.width/2 - title_screen.width/2, 0)
+title_screen.draw()
+
+window.update()
+
 GD.set_window(window)
-GD.set_screen(window)
 
 menu = Menu()
 menu.loop()
